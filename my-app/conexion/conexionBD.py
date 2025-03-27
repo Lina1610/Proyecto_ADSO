469861d147196c7757d551a7da41133e9e5efb11
Lina1610 <lescobarhiguera@gmail.com>
2025-03-27 16:18:17 -0500

    Nuevos cambios panel Administrativo

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -10,8 +10,8 @@ def connectionBD():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            passwd="admin",
-            database="hojas",        
+            passwd="",
+            database="crud_python",        
             charset='utf8mb4',
             collation='utf8mb4_unicode_ci',
             raise_on_warnings=True
