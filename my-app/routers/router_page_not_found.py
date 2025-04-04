cf09edcf454a7d4f83ec49dde5e21c5bccc37a0e
juan2005sebastian <juansebastian812005@mail.com>
2025-04-04 17:35:52 -0500

    Subiendo cambios

--- my-app/routers/router_page_not_found.py
+++ my-app/routers/router_page_not_found.py
@@ -6,6 +6,6 @@ from flask import request, session, redirect, url_for
 @app.errorhandler(404)
 def page_not_found(error):
     if 'conectado' in session and request.method == 'GET':
-        return redirect(url_for('inicio'))
+        return redirect(url_for('loginCliente'))
     else:
-        return redirect(url_for('inicio'))
+        return redirect(url_for('loginCliente'))
